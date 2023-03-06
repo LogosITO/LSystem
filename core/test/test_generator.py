4321d373253da305403c89f078618615bc585c1f
Peter <artuhovpetr35@gmail.com>
2023-03-06 20:03:28 +0300

    fix + rework: Big fix the work with lsystem rules and some fixes with tests

--- core/test/test_generator.py
+++ core/test/test_generator.py
@@ -2,8 +2,9 @@ import unittest
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
+from utils import IPair
+from rule import parse_rule
 import generator as gen   # type: ignore # noqa: E402
-
 
 class SettersPatternCase(unittest.TestCase):
     def ure_handler_test(self):
@@ -13,5 +14,46 @@ class SettersPatternCase(unittest.TestCase):
             self.assertRaises(ValueError, gen.URE_handler, (10, -2))
         with self.subTest():
             self.assertRaises(ValueError, gen.URE_handler, (10, 2))
+    
+class MethodsTestCase(unittest.TestCase):
+    test_gen = gen.BaseRandomLSystemGenerator()
 
-    # TODO: do tests for generating rules)
+    def generate_alphabet_test(self):
+        with self.subTest():
+            self.test_gen._alphabet_len_range = IPair(1, 2)
+            self.test_gen._result_lsystem.alphabet = \
+                self.test_gen.generate_alphabet()
+            self.assertGreater(len(self.test_gen._result_lsystem.alphabet, 0))
+        with self.subTest():
+            self.test_gen._alphabet_len_range = IPair(1, 3)
+            self.test_gen._result_lsystem.alphabet = \
+                self.test_gen.generate_alphabet()
+            self.assertLess(len(self.test_gen._result_lsystem.alphabet, 4))
+    
+    def generate_axiom_test(self):
+        with self.subTest():
+            self.test_gen._axiom_len_range = IPair(1, 2)
+            self.test_gen._result_lsystem.axiom = \
+                self.test_gen.generate_axiom()
+            self.assertGreater(len(self.test_gen._result_lsystem.axiom, 0))
+        with self.subTest():
+            self.test_gen._axiom_len_range = IPair(1, 3)
+            self.test_gen._result_lsystem.axiom = \
+                self.test_gen.generate_axiom()
+            self.assertLess(len(self.test_gen._result_lsystem.axiom, 4))
+        
+    def generate_random_rule_test(self):
+        with self.subTest():
+            r = self.test_gen.generate_random_rule()
+            self.assertRegexpMatches(r)
+        with self.subTest():
+            r = self.test_gen.generate_random_rule()
+            self.assertLess(len(r), 
+                            self.test_gen._rule_length_range.second + 1)
+        with self.subTest():
+            r = self.test_gen.generate_random_rule()
+            self.assertGreater(len(r), 
+                               self.test_gen._rule_length_range.first - 1)
+
+    """ TODO: Do more stable tests for BaseRandomLSystemGenerator 
+              Do some tests for TreeGenerator """
--- core/test/test_rule.py
+++ core/test/test_rule.py
@@ -5,6 +5,15 @@ sys.path.append(str(Path(__file__).parent.parent))
 import rule  # type: ignore # noqa: E402
 
 
+data = ['J[0.5]->JJ', 'A<F[0.5]->FF',
+            'A<B>A->FB', '!A<K>!B->KBAA',
+            'A<F>!A->FF', 'J->F+-A']
+
+
+grwb = lambda val: rule.get_rules_with_base(val, data)
+gfrwb = lambda val: rule.get_first_rule_with_base(val, data)
+
+
 class RulePatternTestCase(unittest.TestCase):
     def test_good_pattern(self):
         with self.subTest():
@@ -28,34 +37,37 @@ class RulePatternTestCase(unittest.TestCase):
 
 
 class RuleMethodsTestCase(unittest.TestCase):
-    data = ['J[0.5]->JJ', 'A<F[0.5]->FF',
-            'A<B>A->FB', '!A<K>!B->KBAA',
-            'A<F>!A->FF']
 
-    def test_searching(self):
+    def test_grwb(self):
         with self.subTest():
-            case_value = rule.give_rule_with_base('F', self.data)
-            self.assertEqual(case_value, self.data[1])
+            self.assertEqual(grwb('F'), [data[1], data[-2]])
         with self.subTest():
-            case_value = rule.give_rule_with_base('I', self.data)
-            self.assertEqual(case_value, None)
+            self.assertEqual(grwb('I'), [])
         with self.subTest():
-            case_value = rule.give_rule_with_base('J', self.data)
-            self.assertEqual(case_value, self.data[0])
+            self.assertEqual(grwb('J'), [data[0], data[-1]])
         with self.subTest():
-            case_value = rule.give_rule_with_base('K', self.data)
-            self.assertEqual(case_value, self.data[3])
+            self.assertEqual(grwb('K'), [data[3]])
+        
+    def test_gfrwb(self):
+        with self.subTest():
+            self.assertEqual(gfrwb('F'), data[1])
+        with self.subTest():
+            self.assertEqual(gfrwb('I'), None)
+        with self.subTest():
+            self.assertEqual(gfrwb('J'), data[0])
+        with self.subTest():
+            self.assertEqual(gfrwb('K'), data[3])
 
     def test_requirements_checker(self):
         with self.subTest():
             test_state = 'ABAAA'
-            val = rule.check_pos_requirements(self.data[2], test_state, 1)
+            val = rule.check_pos_requirements(data[2], test_state, 1)
             self.assertEqual(val, True)
         with self.subTest():
             test_state = 'AAAAA'
-            val = rule.check_pos_requirements(self.data[2], test_state, 1)
+            val = rule.check_pos_requirements(data[2], test_state, 1)
             self.assertEqual(val, False)
         with self.subTest():
             test_state = 'AFBA'
-            val = rule.check_pos_requirements(self.data[4], test_state, 1)
+            val = rule.check_pos_requirements(data[4], test_state, 1)
             self.assertEqual(val, True)
